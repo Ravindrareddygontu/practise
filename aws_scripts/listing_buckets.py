e80b52aabf5b7ed3d33b7ebdcc506d5390d2bab0
Ravindrareddygontu <102969952+Ravindrareddygontu@users.noreply.github.com>
2022-11-17 22:58:53 +0530

    Delete aws_scripts directory

--- aws_scripts/listing_buckets.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-client = boto3.client(
-    's3',
-    aws_access_key_id='AKIAYCI6QZFEMVJVRINF',
-    aws_secret_access_key='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',
-
-)
-response = client.list_buckets(
